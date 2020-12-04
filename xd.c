6166c28dcabf43f467eb9a702ae4bb564022c0c8
Hajime Suzuki <suzuki.hajime.s@gmail.com>
2020-12-04 21:42:14 +0900

    add aarch64 target

--- xd.c
+++ xd.c
@@ -18,7 +18,47 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <x86intrin.h>
+
+#if defined(__x86_64__)
+#  include <x86intrin.h>
+#  define vec_t						__m128i
+#  define _broadcast(_i)			( _mm_set1_epi8(_i) )
+#  define _cast_to_vec(_i)			( _mm_cvtsi64_si128(_i) )
+#  define _load(_p)					( _mm_loadu_si128((__m128i const *)(_p)) )
+#  define _store(_p, _v)			{ _mm_storeu_si128((__m128i *)(_p), (_v)); }
+#  define _unpack_nibble_fw(_x)		({ __m128i y = _mm_cvtepu8_epi16((_x)); y = _mm_and_si128(mv, _mm_or_si128(y, _mm_slli_epi16(y, 4))); y; })
+#  define _unpack_nibble_low(_x)	({ __m128i y = _mm_cvtepu8_epi16((_x)); y = _mm_srli_epi16(_mm_or_si128(y, _mm_slli_epi16(y, 12)), 4); y; })
+#  define _unpack_nibble_high(_x)	({ __m128i y = _mm_unpackhi_epi8((_x), _mm_setzero_si128()); y = _mm_srli_epi16(_mm_or_si128(y, _mm_slli_epi16(y, 12)), 4); y; })
+#  define _asciibase(_x)			( _mm_shuffle_epi8(cv, (_x)) )
+#  define _isascii(_x)				( _mm_cmpgt_epi8(_mm_add_epi8((_x), tv), sv) )
+#  define _select(_m, _y, _x)		( _mm_blendv_epi8((_x), (_y), (_m)) )		/* m ? y : x */
+#  define _interleave_low(_x)		( _mm_cvtepu16_epi32((_x)) )
+#  define _interleave_high(_x)		( _mm_cvtepu16_epi32(_mm_srli_si128((_x), 8)) )
+#  define _flip(_x, _d)				( _mm_shuffle_epi8((_x), _mm_sub_epi8(fv, _mm_set1_epi8(16 - (_d)))) )
+#  define _finish(_x)				( _mm_add_epi8(sv, (_x)) )
+
+#elif defined(__ARM_NEON__)
+#  include <arm_neon.h>
+#  define vec_t						uint8x16_t
+#  define _broadcast(_i)			( vdupq_n_u8(_i) )
+#  define _cast_to_vec(_i)			( vsetq_lane_u64((_i), vmovq_n_u64(0), 0) )
+#  define _load(_p)					( vld1q_u8((uint8_t const *)(_p)) )
+#  define _store(_p, _v)			{ vst1q_u8((uint8_t *)(_p), (_v)); }
+#  define _unpack_nibble_fw(_x)		({ uint8x16_t y = vreinterpretq_u16_u8(vzip1q_u8((_x), vshrq_n_u8((_x), 4))); y = vandq_u8(mv, y); y; })
+#  define _unpack_nibble_low(_x)	({ uint8x16_t y = vreinterpretq_u16_u8(vzip1q_u8(vshrq_n_u8((_x), 4), (_x))); y = vandq_u8(mv, y); y; })
+#  define _unpack_nibble_high(_x)	({ uint8x16_t y = vreinterpretq_u16_u8(vzip2q_u8(vshrq_n_u8((_x), 4), (_x))); y = vandq_u8(mv, y); y; })
+#  define _asciibase(_x)			( vqtbl1q_u8(cv, (_x)) )
+#  define _isascii(_x)				( vreinterpretq_s8_u8(vcgtq_s8(vreinterpretq_u8_s8(vaddq_u8((_x), tv)), vreinterpretq_u8_s8(sv))) )
+#  define _select(_m, _y, _x)		( vbslq_u8((_m), (_y), (_x)) )		/* m ? y : x */
+#  define _interleave_low(_x)		( vreinterpretq_u32_u8(vzip1q_u16(vreinterpretq_u8_u16(_x), vmovq_n_u16(0))) )
+#  define _interleave_high(_x)		( vreinterpretq_u32_u8(vzip2q_u16(vreinterpretq_u8_u16(_x), vmovq_n_u16(0))) )
+#  define _flip(_x, _d)				( vqtbl1q_u8((_x), vsubq_u8(fv, vmovq_n_u8(16 - (_d)))) )
+#  define _finish(_x)				( vaddq_u8(sv, (_x)) )
+
+#else
+#  error "unknown architecture."
+
+#endif
 
 static inline
 size_t conv(
@@ -35,40 +75,27 @@ size_t conv(
 	static uint8_t const flip_table[16] __attribute__(( aligned(16) )) = {
 		15, 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 	};
-	__m128i const cv = _mm_load_si128((__m128i const *)conv_table);
-	__m128i const fv = _mm_load_si128((__m128i const *)flip_table);
-	__m128i const tv = _mm_set1_epi8(1), mv = _mm_set1_epi8(0x0f);
-	__m128i const sv = _mm_set1_epi8(' '), dv = _mm_set1_epi8('.');
-
-	#define _load(_p)					( _mm_loadu_si128((__m128i const *)(_p)) )
-	#define _store(_p, _v)				{ _mm_storeu_si128((__m128i *)(_p), (_v)); }
-	#define _unpack_nibble_fw(_x)		({ __m128i y = _mm_cvtepu8_epi16((_x)); y = _mm_and_si128(mv, _mm_or_si128(y, _mm_slli_epi16(y, 4))); y; })
-	#define _unpack_nibble_low(_x)		({ __m128i y = _mm_cvtepu8_epi16((_x)); y = _mm_srli_epi16(_mm_or_si128(y, _mm_slli_epi16(y, 12)), 4); y; })
-	#define _unpack_nibble_high(_x)		({ __m128i y = _mm_unpackhi_epi8((_x), _mm_setzero_si128()); y = _mm_srli_epi16(_mm_or_si128(y, _mm_slli_epi16(y, 12)), 4); y; })
-	#define _asciibase(_x)				( _mm_shuffle_epi8(cv, (_x)) )
-	#define _isascii(_x)				( _mm_cmpgt_epi8(_mm_add_epi8((_x), tv), sv) )
-	#define _select(_m, _y, _x)			( _mm_blendv_epi8((_x), (_y), (_m)) )		/* m ? y : x */
-	#define _interleave_low(_x)			( _mm_cvtepu16_epi32((_x)) )
-	#define _interleave_high(_x)		( _mm_cvtepu16_epi32(_mm_srli_si128((_x), 8)) )
-	#define _flip(_x, _d)				( _mm_shuffle_epi8((_x), _mm_sub_epi8(fv, _mm_set1_epi8(16 - (_d)))) )
-	#define _finish(_x)					( _mm_add_epi8(sv, (_x)) )
+	vec_t const cv = _load(conv_table);
+	vec_t const fv = _load(flip_table);
+	vec_t const tv = _broadcast(1),   mv = _broadcast(0x0f);
+	vec_t const sv = _broadcast(' '), dv = _broadcast('.');
 
 	uint8_t *p = dst;
 
 	/* print offset */
-	__m128i ofsv = _finish(_flip(
-		_asciibase(_unpack_nibble_fw(_mm_cvtsi64_si128(ofs))),
+	vec_t const ofsv = _finish(_flip(
+		_asciibase(_unpack_nibble_fw(_cast_to_vec(ofs))),
 		addr_digits
 	));
 	_store(p, ofsv);
 	p += addr_digits + 2;							/* spaced by two */
 
 	/* print values */
-	__m128i ar = _load(src);
-	__m128i all = _finish(_interleave_low(_asciibase(_unpack_nibble_low(ar))));
-	__m128i alh = _finish(_interleave_high(_asciibase(_unpack_nibble_low(ar))));
-	__m128i ahl = _finish(_interleave_low(_asciibase(_unpack_nibble_high(ar))));
-	__m128i ahh = _finish(_interleave_high(_asciibase(_unpack_nibble_high(ar))));
+	vec_t const ar = _load(src);
+	vec_t const all = _finish(_interleave_low(_asciibase(_unpack_nibble_low(ar))));
+	vec_t const alh = _finish(_interleave_high(_asciibase(_unpack_nibble_low(ar))));
+	vec_t const ahl = _finish(_interleave_low(_asciibase(_unpack_nibble_high(ar))));
+	vec_t const ahh = _finish(_interleave_high(_asciibase(_unpack_nibble_high(ar))));
 
 	_store(p, all); p += 16;
 	_store(p, alh); p += 16;
@@ -76,7 +103,7 @@ size_t conv(
 	_store(p, ahh); p += 16;
 
 	/* print ascii */
-	__m128i pv = _select(_isascii(ar), ar, dv);
+	vec_t const pv = _select(_isascii(ar), ar, dv);
 	_store(p, pv); p += 16;
 	*p++ = '\n';
 	return(p - dst);
